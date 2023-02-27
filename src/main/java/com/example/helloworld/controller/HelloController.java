f72411e058858765cfcf5f6152c6985aff9fa9ac
yuriamao <1249727496@qq.com>
2023-02-27 15:23:39 +0800

    2月27日

---
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.helloworld.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class HelloController {
+    //http://localhost:8081/hello
+    //@GetMapping("/hello")
+    @RequestMapping(value = "/posttest1",method = RequestMethod.POST)
+    //前端参数，昵称
+    public String Hello(@RequestParam(value = "id",required = false) String Nickname, String number){
+        System.out.println(Nickname);
+        return "链接成功 "+Nickname;
+    }
+}
